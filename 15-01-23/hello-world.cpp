247ef410f2895c37dfd6ee1c3b8aeefc72494732
htet-wai <hwai537@gmail.com>
2023-01-19 01:38:32 +0630

    First commit

---
+++ 15-01-23/hello-world.cpp
@@ -0,0 +1,21 @@
+// C++ program to run Hello World 
+
+//Header file forinput output functions
+
+#include<iostream>
+
+using namespace std;
+
+//main function 
+// the program begins 
+
+int main() {
+
+//print hello world 
+
+   cout << " Hello WOrld" << endl;
+
+   return 0;
+
+}
+
